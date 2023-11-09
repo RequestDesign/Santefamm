6a97fa5d22cd234b7d7e2a7cc65a43e30fe7c4a9
DiMoNiO04 <dima.razumov.940@mail.ru>
2023-11-09 23:15:05 +0300

    init: eslint and structure folders

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'import/prefer-default-export': 'off',
+    'no-param-reassign': 'off',
+    'import/extensions': 'off',
+    'no-undef': 'off',
+    'no-underscore-dangle': 'off',
+  },
+};
