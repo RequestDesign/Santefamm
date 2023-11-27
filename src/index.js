8c976a03c3dd49485e8d296a3d053ac98767b52e
DiMoNiO04 <dima.razumov.940@mail.ru>
2023-11-27 08:16:12 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // Components
 import './js/components/swipers.js';
+import './js/components/content.js';
 
 // Pages
 import './js/pages/contacts.js';
---
+++ src/js/components/content.js
@@ -0,0 +1,21 @@
+if (document.querySelectorAll('[data-content-open]')) {
+  const btnsOpen = document.querySelectorAll('[data-content-open]');
+  const contents = document.querySelectorAll('[data-content-block]');
+
+  btnsOpen.forEach((btnOpen) => {
+    btnOpen.addEventListener('click', function () {
+      btnsOpen.forEach((btn) => btn.classList.remove('active'));
+      this.classList.add('active');
+
+      const attr = this.getAttribute('data-content-open');
+
+      contents.forEach((content) => content.classList.remove('active'));
+
+      contents.forEach((cont) => {
+        if (cont.getAttribute('data-content-block') === attr) {
+          cont.classList.add('active');
+        }
+      });
+    });
+  });
+}
