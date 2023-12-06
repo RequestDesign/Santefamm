100c3b9a3cde1a4a6b9df1dc98f79390bec86177
Dima <ctaricc@mail.ru>
2023-12-06 23:26:59 +0300

    feat: request modal

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 // Components
+import $ from 'jquery';
+window.jQuery = $;
+window.$ = $;
+
 import './js/components/swipers.js';
 import './js/components/content.js';
+import './js/components/modal';
 
 // Pages
 import './js/pages/contacts.js';
