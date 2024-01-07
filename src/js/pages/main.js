76e30b2e063d8f8f666f5a7c060c3ca326b16f0c
ukik0 <ctaricc@mail.ru>
2024-01-07 16:15:07 +0300

    feat: main page 3-rd stage

--- src/js/pages/main.js
+++ src/js/pages/main.js
@@ -10,8 +10,13 @@ $('.layer__content-bottom-reverse').on('click', function () {
     $(this).toggleClass('--active');
 
     resetActiveClasses();
+    resetDotsActiveClasses();
+    resetCounterActiveClasses();
+    resetDotActiveClasses();
+    resetDotSliderActiveClasses();
 
     $('.neutral__wrapper').addClass('--active');
+    $('.neutral__service').addClass('--active');
 });
 
 //face or body side
@@ -26,8 +31,13 @@ $('.layer__content-bottom-view').each(function (index, view) {
         body.toggleClass('--active', !isFace);
 
         resetActiveClasses();
+        resetDotsActiveClasses();
+        resetDotActiveClasses();
+        resetCounterActiveClasses();
+        resetDotSliderActiveClasses();
 
         $('.neutral__wrapper').addClass('--active');
+        $('.neutral__service').addClass('--active');
     });
 });
 
@@ -36,18 +46,22 @@ $('.layer__content-problems-item').each(function (index, problem) {
     $(problem).on('click', function () {
         const dots = $(this).parent().find('.layer__content-problems-dots');
         const neutralWrapper = $('.neutral__wrapper');
+        const neutralService = $('.neutral__service');
 
         neutralWrapper.attr('class', 'neutral__wrapper');
 
         if ($(dots).hasClass('--active')) {
             neutralWrapper.addClass('--active');
+            $(this).find('.layer__content-problems-item-counter').removeClass('--active');
+            resetDotActiveClasses();
+            resetDotSliderActiveClasses();
         } else {
-            $('.layer__content-problems-dots').each(function () {
-                if ($(this).hasClass('--active')) {
-                    $(this).removeClass('--active');
-                }
-            });
+            resetDotsActiveClasses();
+            resetCounterActiveClasses();
+            $(this).find('.layer__content-problems-item-counter').addClass('--active');
         }
+
+        neutralService.addClass('--active');
         dots.toggleClass('--active');
     });
 });
@@ -59,4 +73,52 @@ function resetActiveClasses() {
     });
 
     $('.neutral__wrapper').removeClass('--active');
+    $('.neutral__service').removeClass('--active');
+}
+
+//show slider on click to dot
+
+$('.layer__content-problems-dot').each(function (index, dot) {
+    $(dot).on('click', function () {
+        const neutralService = $('.neutral__service');
+
+        neutralService.attr('class', 'neutral__service');
+
+        $('.layer__content-problems-dot').each(function () {
+            if ($(this).hasClass('--active')) {
+                $(this).removeClass('--active');
+                $(this).find('.layer__content-problems-dot-slider').removeClass('--active');
+            }
+        });
+
+        $(dot).find('.layer__content-problems-dot-slider').addClass('--active');
+        $(dot).addClass('--active');
+    });
+});
+
+function resetCounterActiveClasses() {
+    $('.layer__content-problems-item-counter').each(function () {
+        if ($(this).hasClass('--active')) {
+            $(this).removeClass('--active');
+            $('.neutral__service').addClass('--active');
+        }
+    });
+}
+
+function resetDotsActiveClasses() {
+    $('.layer__content-problems-dots').each(function () {
+        $(this).removeClass('--active');
+    });
+}
+
+function resetDotActiveClasses() {
+    $('.layer__content-problems-dot').each(function () {
+        $(this).removeClass('--active');
+    });
+}
+
+function resetDotSliderActiveClasses() {
+    $('.layer__content-problems-dot-slider').each(function () {
+        $(this).removeClass('--active');
+    });
 }
